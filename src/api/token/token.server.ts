312a344af83e2f5c202971ca68a2c670a901a26e
Павел <Pavel@mail.ru>
2021-07-18 01:20:33 +0400

    update token date live

--- src/api/token/token.server.ts
+++ src/api/token/token.server.ts
@@ -10,24 +10,52 @@ export class TokenService {
     private tokenRepository: Repository<Token>,
   ) {}
   // Проверить токен на валидность
-  checkToken(token: Token) {
-    if (token) {
-      return new Date().valueOf() < token.date.valueOf() && token.active;
+  checkToken(token: Token): boolean {
+    return new Date().valueOf() < token.date.valueOf();
+  }
+  checkUpdateDateToken(token: Token) {
+    const date = new Date();
+    return (
+      date.valueOf() >
+      token.date
+        .setHours(
+          token.date.getHours() -
+            parseInt(process.env.CHECK_UPDATE_TOKEN_HOURS),
+        )
+        .valueOf()
+    );
+  }
+  // обновить дату жизни токену
+  async updateDateToken(token: Token) {
+    if (this.checkUpdateDateToken(token)) {
+      const date = this.generationDateToken();
+      console.log('5 часов токен протухнет');
+      // если через после 5 часов токен протухнет обновить его
+      await this.tokenRepository.update(
+        { id: token.id },
+        {
+          date: date,
+        },
+      );
     }
   }
-  validateToken(token: Token) {
+  // валидация токена
+  async validateToken(token: Token) {
     if (!token.active) {
+      // токен не активен
       this.tokenNoActive();
     }
     if (!this.checkToken(token)) {
+      // токен время жизни
       this.tokenNoDate();
     }
+    // токен валиден проверить требуется ли продливать жизнь токену
+    await this.updateDateToken(token);
   }
 
   // создать токен
   async postToken(id: number): Promise<Token> {
-    const date = new Date();
-    date.setDate(date.getDate() + 1);
+    const date = this.generationDateToken();
     return await this.tokenRepository.save({
       value: jwt.sign(
         new Date().toTimeString() + id.toString(),
@@ -35,6 +63,11 @@ export class TokenService {
       ),
       date: date,
     });
+  }
+  generationDateToken() {
+    const date = new Date();
+    date.setDate(date.getDate() + parseInt(process.env.LIVE_TOKEN_DAY));
+    return date;
   }
   tokenNoValidate(): void {
     throw new HttpException('Токен не валиден', HttpStatus.FORBIDDEN);
