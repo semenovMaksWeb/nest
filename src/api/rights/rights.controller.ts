5346a22e370f4f30521855f048be9de9368fd3f1
Павел <Pavel@mail.ru>
2021-08-15 19:29:46 +0400

    rights

--- src/api/rights/rights.controller.ts
+++ src/api/rights/rights.controller.ts
@@ -1,14 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post, ValidationPipe } from '@nestjs/common';
 import { RightsService } from './rights.server';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
-
+import { RightsSaveDto } from './rights.dto/rights-save.dto';
 @Controller('rights')
 @ApiTags('rights')
 @ApiBearerAuth()
-export class CategoriesController {
+export class RightsController {
   constructor(private readonly rightsService: RightsService) {}
   @Get('/all')
-  async rightsServiceAll() {
+  async rightsAll() {
     return await this.rightsService.getRightsAll();
+  }
+  @Post('')
+  async rightsSave(@Body(ValidationPipe) rightsSaveDto: RightsSaveDto) {
+    return await this.rightsService.saveRights(rightsSaveDto);
   }
 }
