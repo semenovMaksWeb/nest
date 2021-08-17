97a4c4afcf246e1f4775f02bf1a4aaecd8166504
Павел <Pavel@mail.ru>
2021-08-18 01:05:06 +0400

    name url user

---
+++ src/name/nameApi/User.ts
@@ -0,0 +1,8 @@
+export const User = {
+  postUser: { type: 'POST', name: '/' },
+  postUserToken: { type: 'POST', name: '/' },
+  getUserAll: { type: 'get', name: '/all' },
+  getUserToken: { type: 'get', name: '/profile' },
+  updateUserProfile: { type: 'put', name: '/profile' },
+};
+export const nameController = 'users';
