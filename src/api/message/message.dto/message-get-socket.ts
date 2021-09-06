0d3942a930dbb258bbc3dced348532120f7a1619
Павел <Pavel@mail.ru>
2021-09-06 21:34:23 +0400

    chat add

--- src/api/message/message.dto/message-get-socket.ts
+++
@@ -1,9 +0,0 @@
-import { ApiModelProperty } from '@nestjs/swagger/dist/decorators/api-model-property.decorator';
-import { IsNotEmpty, IsNumber } from 'class-validator';
-
-export class MessageGetSocket {
-  @ApiModelProperty()
-  // @IsNumber({}, { message: 'id чата должен является строкой' })
-  @IsNotEmpty({ message: 'id чата обязательное поле' })
-  id: number;
-}
