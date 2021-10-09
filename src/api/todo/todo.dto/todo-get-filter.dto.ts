0e5e6ecc2399eaf6fe8f5ed6ab1f55ae84fdb5e2
Semenov Maksim <Pavel@mail.ru>
2021-10-09 14:27:02 +0400

    todo find in categories

--- src/api/todo/todo.dto/todo-get-filter.dto.ts
+++ src/api/todo/todo.dto/todo-get-filter.dto.ts
@@ -1,9 +1,14 @@
 import { QueryPagination } from 'src/lib/api/pagination';
 import { ApiModelProperty } from '@nestjs/swagger/dist/decorators/api-model-property.decorator';
 import {
+  IsArray,
+  IsBoolean,
   IsOptional,
   IsString,
+  ValidateNested,
 } from 'class-validator';
+import { CategoriesGetTodoDto } from 'src/api/categories/categories.dto/categories-get-todo.dto';
+import { Type } from 'class-transformer';
 
 export class TodoGetFilterDto extends QueryPagination {
   @ApiModelProperty({ required: false })
@@ -12,7 +17,12 @@ export class TodoGetFilterDto extends QueryPagination {
   title: string;
 
   @ApiModelProperty({ required: false })
-  //@IsBoolean({ message: 'активность задач является булевым значением' })
+  // @IsBoolean({ message: 'активность задач является булевым значением' })
   @IsOptional()
   active: boolean;
+
+  @ApiModelProperty({ required: false  })
+  @IsOptional()
+  categories: string;
+
 }
--- src/api/todo/todo.server.ts
+++ src/api/todo/todo.server.ts
@@ -1,5 +1,5 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
-import { Repository, SelectQueryBuilder } from 'typeorm';
+import { In, Repository, SelectQueryBuilder } from 'typeorm';
 import { Todo } from './todo.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { TodoCreateDto } from './todo.dto/todo-create.dto';
@@ -16,7 +16,8 @@ export class TodoService {
     @InjectRepository(Todo)
     private todoRepository: Repository<Todo>,
     private categoriesService: CategoriesService,
-  ) {}
+  ) { }
+  // Создать todo
   async postTodoUser(todoCreateDto: TodoCreateDto, userId: number) {
     todoCreateDto.categories = await this.categoriesService.saveCategoriesTodo(
       todoCreateDto.categories,
@@ -24,6 +25,7 @@ export class TodoService {
     await this.todoRepository.save({ ...todoCreateDto, user: { id: userId } });
     return 'Задача успешно добавлена';
   }
+  // изменить активность todo users
   async updateTodoActiveUser(
     id: number,
     todoUpdateActiveDto: TodoUpdateActiveDto,
@@ -37,7 +39,7 @@ export class TodoService {
       callbackFalse: 'Указанная задача не найдена',
     });
   }
-
+  // изменить todo users
   async updateTodoUser(id: number, todoUpdateDto: TodoUpdateDto) {
     if (await this.getTodoId(id)) {
       todoUpdateDto.categories =
@@ -50,21 +52,22 @@ export class TodoService {
       this.errors404Todo();
     }
   }
-
+  // получить todo users по todo-id 
   async getTodoId(id: number) {
-    return await this.todoRepository.findOne(id);
+    return await this.todoRepository.findOne({ where: { id }, relations: ["categorires"] });
   }
-
+  // получить todo users
   async getTodoUser(userId: number, query: TodoGetFilterDto) {
     const { sqlCreate } = this.getTodoFunc(query);
     sqlCreate.where({ user: { id: userId } });
     return await this.getTodoFuncEnd(sqlCreate);
   }
+  // получить todo all admin
   async getTodoAll(query: TodoGetFilterDto) {
     const { sqlCreate } = this.getTodoFunc(query);
     return await this.getTodoFuncEnd(sqlCreate);
   }
-
+  // дефолтные параметры получение todos начало запроса
   getTodoFunc(query: TodoGetFilterDto) {
     const { skip, take } = Pagination(query?.limit, query?.page);
     const sqlCreate = this.todoRepository
@@ -85,6 +88,7 @@ export class TodoService {
     this.addSqlWhereCategories(sqlCreate);
     return { sqlCreate };
   }
+  // дефолтные параметры получение todos конец запроса
   async getTodoFuncEnd(sqlCreate: SelectQueryBuilder<Todo>) {
     const [list, count] = await sqlCreate.getManyAndCount();
     return {
@@ -92,25 +96,56 @@ export class TodoService {
       count,
     };
   }
-
+  // add запрос todo поиск по title
   addSqlWhereTitle(sqlCreate: SelectQueryBuilder<Todo>, title?: string) {
     if (title) {
       sqlCreate.andWhere('todo.title LIKE :title', { title: `%${title}%` });
     }
   }
+  // add запрос   todo поиск по активности
   addSqlWhereActive(sqlCreate: SelectQueryBuilder<Todo>, active?: boolean) {
     if (active) {
       sqlCreate.andWhere('todo.active= :active', { active: active });
     }
   }
+  // add запрос  todo поиск 
   addSqlWhereCategories(sqlCreate: SelectQueryBuilder<Todo>) {
     sqlCreate.innerJoin('todo.categories', 'categories');
   }
+  // add запрос поиск todo по categories-id
+  async addSqlWhereCategoriesId(sqlCreate: SelectQueryBuilder<Todo>, categoriesId?: string) {
+    if (categoriesId) {
+      const categoiresIdArray = categoriesId.split(',').map((e: any) => {
+        if (+e !== NaN) {
+          return +e;
+        } else {
+          this.errors400CategoiresId();
+        }
+      });
+      const ids = await this.addSqlInCategories(categoiresIdArray);
+      sqlCreate.andWhere(`todo.id IN ${ids}`);
+    }
+  }
+  //  запрос для получение всех todo-ids по categories-id
+  addSqlInCategories(categoriesId: number[]) {
+    return this.todoRepository.query(`SELECT DISTINCT "todo"."id" FROM "user"
+      JOIN "todo" ON ("todo"."userId" = "user"."id")
+      JOIN "todo_categories" ON ("todo_categories"."todoId" = "todo"."id")
+      WHERE "todo_categories"."categoriesId" IN (:categories)
+      `, [categoriesId])
+  }
+
 
   errors404Todo() {
     throw new HttpException(
       'Указанная задача не найдена',
       HttpStatus.NOT_FOUND,
+    );
+  }
+  errors400CategoiresId() {
+    throw new HttpException(
+      'Указанная категория не является числом',
+      HttpStatus.BAD_REQUEST,
     );
   }
 }
